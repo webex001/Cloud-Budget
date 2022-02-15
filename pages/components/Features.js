b98055d0c76f28443fc86d72a1bc64a722ba8a0e
David Bobboi Yakubu <55327588+webex001@users.noreply.github.com>
2022-02-15 15:47:35 +0100

    Features section complete

--- pages/components/Features.js
+++ pages/components/Features.js
@@ -2,29 +2,45 @@ import React from 'react';
 
 const Features = () => {
   return(
+    <div className='bg-gray-50 pt-20 pb-48'>
+    <h2 className='text-center text-3xl mb-10'>Features</h2>
       <div className='flex justify-around'>
-          <div className="bg-[url('/images/hand-money.png')]">
-              <div className='bg-white shadow-md'>
-                <h5>Supports All Currencies and Cards</h5>
-                <p>We support all popular currencies and is fully customizable to add</p>
-                <a href='#' className='uppercase text-red-500 text-center'>Read More</a>
+          <div className="">
+              <div className='-z-10'>
+                <img src='/images/hand-money.png' />
+              </div>
+              <div className='bg-white shadow-lg rounded w-80 ml-4 p-5 z-40 absolute -mt-24'>
+                <h5 className='text-center text-xl font-bold'>Supports All Currencies and <br /> Cards</h5>
+                <p className='text-center mt-5'>We support all popular currencies and is fully customizable to add <br /> <br />
+                <a href='#' className='uppercase text-red-500'>Read More</a>
+                </p>
               </div>
           </div>
-          <div className="bg-[url('/images/mobile-phone.png')]">
-              <div className='bg-white shadow-md'>
-                <h5 className='text-center'>Supports All Currencies and Cards</h5>
-                <p className='text-center'>We support all popular currencies and is fully customizable to add</p>
-                <a href='#' className='uppercase text-red-500 text-center'>Read More</a>
+          <div className="">
+              <div className='-z-10'>
+                <img src='/images/mobile-phone.png' />
+              </div>
+              <div className='bg-white shadow-lg rounded w-80 ml-4 p-5 z-20 absolute -mt-24'>
+                <h5 className='text-center text-xl font-bold'>Manage your expenses on the go</h5>
+                <p className='text-center mt-5'>You can access your account from anywhere in the world on any device <br /> <br />
+                <a href='#' className='uppercase text-red-500'>Read More</a>
+                </p>
               </div>
           </div>
-          <div className="bg-[url('/images/cloud-storage.png')]">
-              <div className='bg-white shadow-md'>
-                <h5 className='text-center'>Supports All Currencies and Cards</h5>
-                <p className='text-center'>We support all popular currencies and is fully customizable to add</p>
-                <a href='#' className='uppercase text-red-500 text-center'>Read More</a>
+          <div className="">
+              <div className='-z-10'>
+                <img src='/images/cloud-storage.png' />
+              </div>
+              <div className='bg-white shadow-lg rounded absolute -mt-24 w-80 ml-4 p-5 z-20'>
+                <h5 className='text-center text-xl font-bold'>Cloud Budget Management Software</h5>
+                <p className='text-center mt-5'>Our software is made so you can access and manage your budget <br /> <br />
+                <a href='#' className='uppercase text-red-500'>Read More</a>
+                </p>
               </div>
           </div>
+          
       </div>
+    </div>
   )
 };
 
--- pages/components/Technology.js
+++ pages/components/Technology.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 const Technology = () => {
   return (
-      <div className='mt-20'>
+      <div className='mt-20 mb-20'>
           <h2 className='text-center text-3xl'>Technology</h2>
           <div className='flex justify-around'>
               <div className='mt-20'>
-                    <h2 className='text-8xl text-gray-200'>01</h2>
-                    <img className='' src='/images/download.png' />
+                    <h2 className='text-8xl text-gray-200 z-0'>01</h2>
+                    <img className='absolute -mt-12 ml-3 z-20' src='/images/download.png' />
               </div>
               <div className='mt-20'>
                     <h2 className='text-8xl text-gray-200'>02</h2>
-                    <img className='' src='/images/search.png' />
+                    <img className='absolute -mt-12 ml-6 z-20' src='/images/search.png' />
               </div>
               <div className='mt-20'>
                     <h2 className='text-8xl text-gray-200'>03</h2>
-                    <img className='' src='/images/pdf.png' />
+                    <img className='absolute -mt-12 ml-6 z-20' src='/images/pdf.png' />
               </div>
               <div className='mt-20'>
                     <h2 className='text-8xl text-gray-200'>04</h2>
-                    <img className='' src='/images/csv.png' />
+                    <img className='absolute -mt-12 ml-9 z-20' src='/images/csv.png' />
               </div>
           </div>
       </div>
